3ec57dc2f90b46aed65c17e59210b09962765636
G-Love53 <G@CommercialInsurance-Direct.com>
2025-12-30 14:52:45 -0700

    Convert css.js to ES6 modules and improve loadGlobalCss
    Refactor css.js to use ES6 modules and add error handling.

--- src/utils/css.js
+++ src/utils/css.js
@@ -1,10 +1,16 @@
-const fs = require('fs');
-const path = require('path');
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
-const loadGlobalCss = () => {
-  const p = path.join(__dirname, '../../templates/assets/common/global-print.css');
-  return fs.existsSync(p) ? fs.readFileSync(p, 'utf8') : '';
-};
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-module.exports = { loadGlobalCss };
-
+export function loadGlobalCss() {
+  try {
+    const cssPath = path.join(__dirname, "../../templates/assets/common/global-print.css");
+    return fs.existsSync(cssPath) ? fs.readFileSync(cssPath, "utf8") : "";
+  } catch (err) {
+    console.error("Error loading global CSS:", err);
+    return "";
+  }
+}
