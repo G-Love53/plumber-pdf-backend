ef6e08fde1edf7afd1026fbfc0b400c0c0ffc99a
G-Love53 <G@CommercialInsurance-Direct.com>
2025-12-30 13:52:31 -0700

    Add loadGlobalCss function to load global CSS

--- src/utils/css.js
+++ src/utils/css.js
@@ -1,1 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+
+const loadGlobalCss = () => {
+  const p = path.join(__dirname, '../../templates/assets/common/global-print.css');
+  return fs.existsSync(p) ? fs.readFileSync(p, 'utf8') : '';
+};
+
+module.exports = { loadGlobalCss };
 
