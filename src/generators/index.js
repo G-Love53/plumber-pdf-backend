9da9187ce701075b24f3fa96ba86089294669f17
G-Love53 <G@CommercialInsurance-Direct.com>
2026-02-09 14:08:33 -0500

    Update index.js

--- src/generators/index.js
+++ src/generators/index.js
@@ -21,7 +21,7 @@ function resolveFormsKey(formId, segment) {
   const id = String(formId || "");
   let m = id.match(/^acord(\d+)$/i);
   if (m) return `ACORD${m[1]}`;
-  if (/^supp_/i.test(id)) return "SUPP_BERKLEY_PLUMBER";
+  if (/^supp_/i.test(id)) return "SUPP_CONTRACTOR";
   return id.toUpperCase();
 }
 function getFormConfigOrThrow(formId, segment) {
